99409e2c8f72ad6a0921858e2a30bbef9aa64d40
Vengicx <kitocx@gmail.com>
2018-06-22 16:31:52 -0300

    Dropdown take 1
    No take 1 do dropdown eu consegui fazer com que apareça o campo de procurar dentro do dropdown, e também coloquei uma barra de rolagem e limitei o height do dropdown!

--- dropdown+search/index.php
+++ dropdown+search/index.php
@@ -1,31 +1,67 @@
 <!doctype html>
 <html lang="en">
-<head>
+  <head>
+    <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/css/bootstrap.min.css" integrity="sha384-9gVQ4dYFwwWSjIDZnLEWnxCjeSWFphJiwGPXr1jddIhOegiu1FwO5qRGvFXOdJZ4" crossorigin="anonymous">
-</head>
-<body>
-<div class="container">
-   <form>
-    	<div class="dropdown">
-			<button onclick="myFunction()" class="dropbtn">Dropdown</button>
-			  <div id="myDropdown" class="dropdown-content">
-			    <input type="text" placeholder="Procurar" id="myInput" aria-labelledby="myDropdown" onkeyup="filterFunction()">
-				    <a href="#about">About</a>
-				    <a href="#base">Base</a>
-				    <a href="#blog">Blog</a>
-				    <a href="#contact">Contact</a>
-				    <a href="#custom">Custom</a>
-				    <a href="#support">Support</a>
-				    <a href="#tools">Tools</a>
-			  </div>
-		</div>
-    </form>
-</div>
 
+    <title>Dropdown do Bootstrap</title>
+    <style>
+    	.dropdown-menu{
+    		overflow: auto !important; /*deixa com a barra de rolagem */
+    		height: 150px; /*deixa com um limite máximo de altura para aparecer as informações */
+    	}
 
-<script src="index.js"></script>
+    	.btn-drop {
+    		border: 2px solid #000fff; /*deixa o input com bordinha */
+    	}
+
+	</style>
+  </head>
+  <body>
+	<div class="dropdown">
+  		<button class="btn btn-primary dropdown-toggle" type="button" onclick="myFunction()" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" id="botao">
+    		Gênero Musical
+  		</button>
+		  <div class="dropdown-menu" aria-labelledby="dropdownMenuButton" id="genero">
+		  	<input type="text" class="btn btn-drop" placeholder="Procurar.." id="procurar" onkeyup="filterFunction()">
+		    <a class="dropdown-item" href="#Metal">Metal</a>
+		    <a class="dropdown-item" href="#Pop">Pop</a>
+		    <a class="dropdown-item" href="#Rock">Rock</a>		    
+		    <a class="dropdown-item" href="#KPop">K-Pop</a>
+		    <a class="dropdown-item" href="#Sertanejo">Sertanejo Raiz</a>
+		    <a class="dropdown-item" href="#Reggae">Reggae</a>		    
+		    <a class="dropdown-item" href="#Blues">Blues</a>
+		    <a class="dropdown-item" href="#Jazz">Jazz</a>
+		    <a class="dropdown-item" href="#Funk">Funk</a>		    
+		    <a class="dropdown-item" href="#JRock">J-Rock</a>
+		  </div>
+	</div>
+<script>
+	// function myFunction() {
+	//     document.getElementById("genero").classList.toggle("show");
+	// }
+
+	function filterFunction() {
+	    var input, filter, ul, li, a, i; //declara variaveis
+	    input = document.getElementById("procurar"); //pega o input com o id "procurar"
+	    filter = input.value.toUpperCase(); //pega o valor que está lá, transforma em maiusculo
+	    div = document.getElementById("genero"); //pega a div toda
+	    a = div.getElementsByTagName("a"); //pega os valores que estão com a tag "a" dentro da div
+	    botao = document.getElementById("botao");
+	    // botao.removeChild(botao);
+	    for (i = 0; i < a.length; i++) {
+	        if (a[i].innerHTML.toUpperCase().indexOf(filter) > -1) {//se o valor de a for encontrado (documentação do indexof, se nao encontrar a informação ele retorna -1) entao...
+	            a[i].style.display = "";//ele aparece
+	        } else {
+	            a[i].style.display = "none";//senao ele some
+	        }
+	    }
+	}
+</script>
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.0/umd/popper.min.js" integrity="sha384-cs/chFZiN24E4KMATLdqdvsezGxaGsi4hLGOzlXwp5UZB1LY//20VyM2taTB4QvJ" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.0/js/bootstrap.min.js" integrity="sha384-uefMccjFJAIv6A+rW+L4AHf99KvxDjWSu1z9VI8SKNVmz4sk7buKt/6v9KI65qnm" crossorigin="anonymous"></script>
